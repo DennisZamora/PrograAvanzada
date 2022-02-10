e80e0a8cc6b123ba9a0ca3ac40b082cead03f104
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-02-10 17:37:05 -0600

    Creation a new project for the task

---
+++ Guia#2/GuiaS02/GuiaS02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuiaS02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
