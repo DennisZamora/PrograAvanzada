3e726d75dd67b2e14956caa6b034df81d9cdcc9b
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-02-09 21:08:19 -0600

    Add NDbContext

--- BackEndCapas/BackEnd/BE.DAL.EF/NDbContext.cs
+++ BackEndCapas/BackEnd/BE.DAL.EF/NDbContext.cs
@@ -19,5 +19,27 @@ namespace BE.DAL.EF
         }
         public virtual DbSet<Categories> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId);
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Description).HasColumnType("ntext");
+
+                entity.Property(e => e.Picture).HasColumnType("image");
+            });
+
+            
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
