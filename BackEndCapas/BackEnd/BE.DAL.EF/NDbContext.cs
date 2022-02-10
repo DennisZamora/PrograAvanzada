7dcf6695c271dc9f8ca998bc79cae65ffac669bf
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-02-09 20:51:57 -0600

    Add controllers,library,class

---
+++ BackEndCapas/BackEnd/BE.DAL.EF/NDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BE.DAL.DO.Objectos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BE.DAL.EF
+{
+    public partial class NDbContext : DbContext
+    {
+        public NDbContext()
+        {
+        }
+
+        public NDbContext(DbContextOptions<NDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Categories> Categories { get; set; }
+
+    }
+}
