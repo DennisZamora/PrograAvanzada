97173b703a4b66869b96f188d1acaf2651617345
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-03-12 18:52:13 -0600

    First Commit

---
+++ QuizS07/QUIZ-DennisZamora/BE.API/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using BE.DAL.DO.Objects;
+using BE.DAL.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly NDbContext _context;
+
+        public CustomersController(NDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customers>>> GetCustomers()
+        {
+            return null;
+            //var res =  new BE.BS.Customers(_context).getAll();
+        }
+    }
+}
